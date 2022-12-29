13f231d6279f7c06972a647491e05f14d0bc494b
Shazil2154 <sattarshazil@gmail.com>
2022-12-29 19:36:42 +0500

    added databse insert method for reservations

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/Shazil2154/-go--bookings-project/internal/config"
 	"github.com/Shazil2154/-go--bookings-project/internal/driver"
@@ -67,11 +69,33 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	sd := r.Form.Get("start_date")
+	ed := r.Form.Get("end_date")
+
+	layout := "2006-01-02"
+
+	startDate, err := time.Parse(layout, sd)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+	endDate, err := time.Parse(layout, ed)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
+	roomID, err := strconv.Atoi(r.Form.Get("room_id"))
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
 	reservation := models.Reservation{
 		FirstName: r.Form.Get("first_name"),
 		LastName:  r.Form.Get("last_name"),
 		Email:     r.Form.Get("email"),
 		Phone:     r.Form.Get("phone"),
+		StartDate: startDate,
+		EndDate:   endDate,
+		RoomID:    roomID,
 	}
 
 	form := forms.New(r.PostForm)
@@ -91,6 +115,12 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 
 		return
 	}
+
+	err = m.DB.InsertReservation(reservation)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
 	m.App.Session.Put(r.Context(), "reservation", reservation)
 
 	http.Redirect(w, r, "/reservation-summary", http.StatusSeeOther)
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,5 +1,9 @@
 package repository
 
+import "github.com/Shazil2154/-go--bookings-project/internal/models"
+
 type DatabaseRepo interface {
 	AllUsers() bool
+
+	InsertReservation(res models.Reservation) error
 }
